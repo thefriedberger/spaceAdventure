f914d4f21f485312f6533bb51422d8279791e5cc
Tyler Friedberg <thefriedberger@gmail.com>
2016-11-11 08:05:44 -0500

    Item class created

---
+++ Items.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Items here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Items
+{
+    private String description;
+    private int weight;
+    
+    public Items() {
+        description = "";
+        weight = 0;
+    }
+}
